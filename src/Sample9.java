48a215f5252413db319fbd88a0610c97e412f8ed
Jack-777e <ttt712017@gmail,com>
2023-10-12 00:17:14 +0800

    HWCH01

---
+++ src/Sample9.java
@@ -0,0 +1,10 @@
+public class Sample9 {
+    public static void main(String[] args) {
+        int num1, num2;
+        num1 = 3;
+        System.out.println("變數num1的值是"+num1);
+        num2 = num1;
+        System.out.println("將變數num1的值指定到num2");
+        System.out.println("變數num2的值是"+num2);
+    }
+}
